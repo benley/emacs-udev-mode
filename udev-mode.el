daa707277b698b996d112a3d3d2b8a0cec3f9db6
Benjamin Staffin <benley@gmail.com>
2020-06-25 18:00:13 -0400

    Add license stuff

--- udev-mode.el
+++ udev-mode.el
@@ -1,6 +1,22 @@
 ;;; udev-mode --- Major mode for editing udev rules files     -*- lexical-binding: t; -*-
+
 ;; Author: Benjamin Staffin <benley@gmail.com>
+;; URL: https://github.com/benley/emacs-udev-mode
 ;; Keywords: udev linux
+;; Package-Requires: ((emacs "24"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
